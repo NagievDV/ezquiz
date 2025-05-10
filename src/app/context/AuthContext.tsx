4f33644b743ab037c8a68197d6911533bea17c79
Dmitriy Goncharov <gdmitry58@gmail.com>
2025-05-10 09:11:20 +0300

    Merge branch 'saved-state'

--- src/app/context/AuthContext.tsx
+++ src/app/context/AuthContext.tsx
@@ -1,46 +1,5 @@
 "use client";
 
-<<<<<<< HEAD
-import { createContext, useContext, useState, useEffect } from "react";
-
-type User = {
-  id: string;
-  name: string;
-  email: string;
-  role: string;
-} | null;
-
-const AuthContext = createContext<{
-  user: User;
-  login: (userData: User) => void;
-  logout: () => void;
-}>({
-  user: null,
-  login: () => {},
-  logout: () => {},
-});
-
-export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState<User>(null);
-
-  useEffect(() => {
-    const savedUser = localStorage.getItem("user");
-    if (savedUser) setUser(JSON.parse(savedUser));
-  }, []);
-
-  const login = (userData: User) => {
-    setUser(userData);
-    localStorage.setItem("user", JSON.stringify(userData));
-  };
-
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem("user");
-  };
-
-  return (
-    <AuthContext.Provider value={{ user, login, logout }}>
-=======
 import {
   createContext,
   useContext,
